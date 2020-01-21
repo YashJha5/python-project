a0e752b914c9bee791f869ace84423f72b52e629
YashJha5 <34125291+YashJha5@users.noreply.github.com>
2020-01-22 00:32:08 +0530

    Update __init__.py

--- HttpTrigger/__init__.py
+++ HttpTrigger/__init__.py
@@ -1,19 +1,29 @@
 import logging
 import json
 import azure.functions as func
+from flask import Flask, render_template, request, redirect
+from flask_mysqldb import MySQL
+
+app = Flask(__name__)
+
+app.config['MYSQL_HOST'] = 'celebal-testdb.mysql.database.azure.com'
+app.config['MYSQL_USER'] = 'yash@celebal-testdb'
+app.config['MYSQL_PASSWORD'] = 'Eric@123'
+app.config['MYSQL_DB'] = 'flaskapp'
+
+mysql = MySQL(app)
+
+def users():
+    with app.app_context():
+       cur = mysql.connection.cursor()
+
+       resultValue = cur.execute("SELECT * FROM users")
+       if resultValue > 0:
+           userDetails = cur.fetchall()
+           return render_template('users.html',userDetails=userDetails)
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    products = [
-        {
-            "name": "Azure DevOps",
+    v = users()
+    return func.HttpResponse(v)
 
-            "price": 4.99
-        },
 
-        {
-
-            "name": "Onion",
-
-            "price": 0.01
-        }
-    ]
-    return func.HttpResponse(json.dumps(products))
+     
